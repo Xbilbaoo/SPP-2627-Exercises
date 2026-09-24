1be1f7595a4c8bcdf73cd0c08c907e9adc6f2041
x_bilbaozam <x_bilbaozam@fpzornotzalh.eus>
2026-09-24 13:45:07 +0200

    Exercise4 Completed

--- src/Multi_processing_programming/Exercise4.java
+++ src/Multi_processing_programming/Exercise4.java
@@ -1,4 +1,48 @@
 package Multi_processing_programming;
 
+import java.io.IOException;
+
 public class Exercise4 {
+
+    static void main() {
+
+        try {
+
+            String classpath = System.getProperty("java.class.path");
+
+            ProcessBuilder pb0 = new ProcessBuilder("java", "-cp", classpath,
+                    "Multi_processing_programming.examples.Sum", "1", "99999999");
+            ProcessBuilder pb1 = new ProcessBuilder("java", "-cp", classpath,
+                    "Multi_processing_programming.examples.Sum", "1", "50000000");
+            ProcessBuilder pb2 = new ProcessBuilder("java", "-cp", classpath,
+                    "Multi_processing_programming.examples.Sum", "50000001", "99999999");
+
+            pb0.inheritIO();
+            pb1.inheritIO();
+            pb2.inheritIO();
+
+            long startTime = System.currentTimeMillis();
+            Process p0 = pb0.start();
+            p0.waitFor();
+            long endTime = System.currentTimeMillis();
+            System.out.printf("1 Prozesua bukatu da denbora honekin: %f\n\n", ((endTime - startTime)/1000.0));
+
+            startTime = System.currentTimeMillis();
+            Process p1 = pb1.start();
+            Process p2 = pb2.start();
+
+            p1.waitFor();
+            p2.waitFor();
+            endTime = System.currentTimeMillis();
+            System.out.printf("2 Prozesua bukatu da denbora honekin: %f", ((endTime - startTime)/1000.0));
+
+
+
+
+
+
+        } catch (IOException | InterruptedException e) { e.printStackTrace(); }
+
+    }
+
 }
